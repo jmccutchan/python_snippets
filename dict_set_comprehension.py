0eb6b60f5b22eaab38c0adc936c2dd46b3b63d54
James McCutchan <mccutchanjames@gmail.com>
2016-05-13 13:36:16 -0700

    more list comprehension examples

--- dict_set_comprehension.py
+++ dict_set_comprehension.py
@@ -1,4 +1,5 @@
 #using list comprehensions instead of map and filter methods
+#as a rule of thumb use no more than two expressions in a list comprehension
 
 chile_ranks = {'ghost':1,'habanero':2,'cayenne':3}
 
@@ -8,6 +9,25 @@ rank_dict = {rank: name for name, rank in chile_ranks.items()}
 #comprehension with sets
 chile_len_set = {len(name) for name in chile_ranks.keys()}
 
+a = [1,2,3,4,5,6,7,8,9,10]
+b = [x for x in a if x > 4 and x % 2 == 0]
+
+print b
 print(rank_dict)
 print(chile_len_set)
+
+matrix = [[1,2,3],[4,5,6],[7,8,9]]
+
+filtered = [[x for x in row if x % 3 == 0]
+            for row in matrix if sum(row) >= 10]
+
+b = [x**2 for row in matrix for x in row]
+
+
+print b
+print filtered
+
+a2 = list(range(100))
+b = [x for x in a2 if x % 2 == 0 if x % 3 == 0]
+print b
 
